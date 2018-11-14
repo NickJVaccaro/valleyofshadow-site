5d71eb460dd9e461f81fa76c4350957dff8829da
NickJVaccaro <njvaccaro0@gmail.com>
2018-11-14 16:33:23 -0500

    Make mailchimp requests with jsonp
    This is to get around issues with CORS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,11 +22,12 @@ export class AppComponent {
     };
 
     var mcUrl = "https://synersteel.us10.list-manage.com/subscribe/post-json?u=3c4255b0f2add1624ed41ab37&id=53531987d9&EMAIL="
-      + this.emailAddress + "&c=?";
+      + this.emailAddress;
 
-    this.http.get(mcUrl)
+    this.showError = false;
+
+    this.http.jsonp(mcUrl, "c")
       .subscribe(response => {
-        console.log(response['result']);
         if(response['result'] == "error") {
           this.showError = true;
           this.errorMessage = "The following error occurred while signing up: " + response['msg'];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
